679e37299fa7def4d3a5dd5c614a58d25d2e801c
daryawhile <daryawhile@gmail.com>
2026-09-15 17:25:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 import os
-
-# ВСТАВЬТЕ СЮДА ВАШ ТОКЕН ОТ @BotFather (вместо ЗДЕСЬ_ТВОЙ_ТОКЕН)
-# Или оставьте как есть, если добавите переменную BOT_TOKEN в настройках Render
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8903309751:AAFBaxbYy5Cjnfn2ZYD1JxiwaL0qP0o539Y")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Эту ссылку мы заменим на реальную после создания приложения на Render
 # Например: https://my-chinese-bot.onrender.com
