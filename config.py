e84439d3e65c54e36ccf183fc3340751a35936da
daryawhile <daryawhile@gmail.com>
2026-09-15 17:37:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Эту ссылку мы заменим на реальную после создания приложения на Render
-# Например: https://my-chinese-bot.onrender.com
-WEBHOOK_URL = "https://my-chinese-bot.onrender.com"
+WEBHOOK_URL = "https://chinese-test-bot.onrender.com"
 WEBHOOK_PATH = "/webhook"
