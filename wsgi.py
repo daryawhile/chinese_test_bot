cf3c0c7b486e321adf786c24864f296f1908dba7
daryawhile <daryawhile@gmail.com>
2026-09-21 21:08:03 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.types import Update
 from config import BOT_TOKEN, WEBHOOK_PATH
-from bot_logic import router, init_db  # <-- ДОБАВИТЬ init_db
+from bot_logic import router, init_db
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,7 +19,7 @@ app = Quart(__name__)
 @app.before_serving
 async def startup():
     await init_db()
-    logger.info("✅ База данных MongoDB успешно инициализирована!")
+    logger.info("✅ База данных Neon успешно инициализирована!")
 
 @app.route(WEBHOOK_PATH, methods=['POST'])
 async def webhook():
